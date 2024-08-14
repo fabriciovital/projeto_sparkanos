fdc04b24a34826dcfae88136f5e47094a40611a7
wallace <wallacecpg@gmail.com>
2024-08-14 17:00:57 +0100

    add test services

--- tests/services.py
+++ tests/services.py
@@ -2,15 +2,13 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
 host = os.getenv('HOST')
-
-print(host)
 
 services = {
     'minio': (f'http://{host}:9001', 'MinIO Console'),
     'superset': (f'http://{host}:8088', 'Superset'),
-    'trino': (f'http://{host}:8080/ui/login.html','Cluster Overview - Trino'),
+    'trino': (f'http://{host}:8080/ui/login.html', 'Cluster Overview - Trino'),
     'jupyter': (f'http://{host}:8889/lab?', 'JupyterLab'),
-    'Airflow': (f'http://{host}:8081/login/', 'Sign In - Airflow')   
+    'Airflow': (f'http://{host}:8081/login/', 'Sign In - Airflow'),  
+    'Nginx': (f'http://{host}:81/login', 'Login – Nginx Proxy Manager')
 }
